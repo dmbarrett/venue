010b575d182da496de94fcaddbcf7a972052e694
Daniel Barrett <dbarred@icloud.com>
2019-04-15 15:34:28 -0400

    initial commit, added material header

---
+++ src/components/header_footer/header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import MenuIcon from '@material-ui/icons/Menu'
+import IconButton from '@material-ui/core/IconButton'
+
+class header extends Component {
+    render() {
+        return (
+            <AppBar
+                position="fixed"
+                style={{
+                    backgroundColor:'#2f2f2f',
+                    boxShadow:'none',
+                    padding: '10px 0'
+                }}
+            >
+                <Toolbar>
+                    <div className="header_logo">
+                        <div className="font_righteous header_logo_venue">the venue</div>
+                        <div className="font_cabin header_logo_title">Musical <br/>Events</div>
+                    </div>
+
+                    <IconButton
+                        aria-label="Menu"
+                        color="inherit"
+                        onClick={()=>{
+                            console.log("opening")
+                        }}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        );
+    }
+}
+
+export default header;
